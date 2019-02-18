6f955266b7d365af620dea6c71cb3dd02cf29cd0
David Feinzimer <dfeinzimer@gmail.com>
2019-02-17 21:49:57 -0800

    2.17.19.21.49

--- unify.py
+++ unify.py
@@ -48,19 +48,67 @@ def unify(E1, E2):
 		if (E1 == E2):
 			return None
 		else:
-			return "Fail: E1 & E2 are not equal"
+			print("Fail: E1 & E2 are not equal")
+			return False
+
+	# If E1 is a variable
+	if (E1[0][0].isupper()):
+		for x in E2:
+			if (x == E1[0]):
+				print("Fail: E1 occurs in E2")
+				return False
+		print("Preparing to swap: ", E1, E2)
+		temp = E1[0]
+		E1[0] = E2[0]
+		E2[0] = temp
+		print("Finished swap: ", E1, E2)
+		return E1, E2
+
+	# If E2 is a variable
+	if (E2[0][0].isupper()):
+		for x in E1:
+			if (x == E2[0]):
+				print("Fail: E2 occurs in E1")
+				return False
+		print("Preparing to swap: ", E1, E2)
+		temp = E1[0]
+		E1[0] = E2[0]
+		E2[0] = temp
+		print("Finished swap: ", E1, E2)
+		return E1, E2
+
+	# Either E1 or E2 are empty
+	if (E1.len() == 0 or E2.len() == 0 ):
+		print("Fail: E1 or E2 are empty")
+		return False
+
+	HE1 = E1[0]
+	HE2 = E2[0]
+	SUBS1 = unify(HE1, HE2)
+	print("SUBS1:", SUBS1)
+	if (SUBS1 == False):
+		print("Fail: SUBS1 returned Fail")
+		return False
 
 
 # Main function
 if __name__ == "__main__":
 	print("Test 1: Empty lists")
-	print(unify([],[]))
+	print("Result:", unify([],[]))
 	print()
 
 	print("Test 2: Nonempty lists")
-	print(unify(["justin","david","stephen"],["Person"]))
+	print("Result:", unify(["justin","david","stephen"],["Person"]))
 	print()
 
 	print("Test 3: Two constant lists")
-	print(unify(["justin","david","stephen"],["adam"]))
+	print("Result:", unify(["justin","david","stephen"],["adam"]))
 	print()
+
+	print("Test 4: Variable exists in two lists")
+	print("Result:", unify(["Person"],["Person", "Person2"]))
+	print()
+
+	#print("Test 5: One empty set")
+	#print("Result:", unify([],["Person"]))
+	#print()
